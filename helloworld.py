0b0594ff09f2c1f530214c643da0f13b79585acb
Adrian Patricio <adrianpatricio@AdrianPatricio-MacBook-Air.local>
2026-01-01 17:27:22 -1000

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
+
+
+main()
+=======
+def hello_world(city):
+    print(f'Hello World {city}!')
+
+
+def main():
+    hello_world('NYC')
+>>>>>>> ce98bfc4452feca2c9e4540cf0da7e20b174ea92
 
 
 main()
